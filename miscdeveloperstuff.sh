f2c01b6159e86364b9a6cca4a0c3301ec4a9f4be
Ced <ced@z>
2018-04-24 15:14:37 +0200

    improved tf, improved replaceAll (but function not yet fully tested, so not available yet)

--- miscdeveloperstuff.sh
+++ miscdeveloperstuff.sh
@@ -9,8 +9,32 @@ _man "getResume" "Get a *kind of* resume from a PHP file - list function names a
 alias getResume='g -n -e "^var .* =\ *" -ie "function" -ie "DEPRECATED" -ie "public" -ie "private" -ie "protected" -ie "class"'
 
 #todo 
-alias tf="echo;echo;tail -f -n 0 -q"
+#alias tf="echo;echo;tail -f -n 0 -q"
 alias tfh="tf | head"
+
+alias tf="tf"
+
+tf(){
+ #  tail -n 0  -f $1 | perl -pe 'BEGIN{$SIG{ALRM} = sub {print "\n\n"}} alarm 3' #|  perl -pe 'BEGIN{$SIG{ALRM} = sub {print ". . . . . . . . . . .  .   .    .\n"}} alarm 10'
+ if [ "$1" == '$1' ]; then
+   files=*;
+ else
+   files=$1;
+ fi
+
+ perl -e '
+  $| = 1;
+  seek STDIN, 0, 2; # uncomment if you want to skip the text that is already there.
+  while (1) {
+     if ($_ = <STDIN>) {
+        print; $t = 0
+     } else {
+        print "\n" if ++$t == 3;
+        print "• · ·  ·   ·    ·      ·          ·\n" if ++$t == 10;
+        sleep 1;
+     }
+  }' < $1
+}
 
 
 
@@ -26,7 +50,19 @@ function replaceAll(){
  read -r -p "Are you sure you would like to : sed -i 's/$1/$2/g'? [y|n]" response
  case "$response" in
   [yY][eE][sS]|[yY]) 
-    sed -i 's/$1/$2/g'; echo "done";
+    #sed -i 's/$1/$2/g'; echo "done";
+    echo to test
+    return
+
+find ./ -type d \( -name .svn -o -name .git \) -prune -o \
+   -type f -exec file {} \; \
+   | grep -o -P '^.+: \w+ text' \
+   | sed 's/: ASCII.*//g' \
+   | xargs sed -i 's/$1/$2/g' {} \; # replace
+
+
+
+
     ;;
   *)
     echo "Aborted"
